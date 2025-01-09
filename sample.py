17f0182d01493131afb027535d909128e50b7381
RuchitJagodara <ruchit.jagodara@iitgn.ac.in>
2025-01-09 10:09:44 +0530

    Fixing pylint errors

--- sample.py
+++ sample.py
@@ -1,10 +1,17 @@
+"""
+This program does something cool !
+"""
+
 def prnt_hello_world():
+    '''This function prints hello world!'''
     print("Hello world!")
 
 def try_multiplying(a,b):
+    '''This functions tries to multiply two things althouhg does not guarantee!'''
     return a*b
 
 def main():
+    '''This is the main function'''
     #This is my main function
     #This code contains sample codes of print hello world and multiplication
 
@@ -22,14 +29,14 @@ def main():
 
     def temp_func(cc, bcc):
         return cc+ bcc
-    
 
-    print("Result of adding 51 and 32 equals:", end=" ")    
+
+    print("Result of adding 51 and 32 equals:", end=" ")
     print(temp_func(51, 32))
 
     d =temp_func(5, 6)
 
-    print("Addign 5 and 6 equals :- ",d)    
+    print("Addign 5 and 6 equals :- ",d)
 
 
 if __name__ == "__main__":
